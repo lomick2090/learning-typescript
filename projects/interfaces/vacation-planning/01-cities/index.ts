6faa3d6420b709ffbe7e18d49f1ac5106d89eb07
lomick2090 <lomick2090@gmail.com>
2023-06-20 12:32:35 -0500

    finish vacation-planning

--- projects/interfaces/vacation-planning/01-cities/index.ts
+++ projects/interfaces/vacation-planning/01-cities/index.ts
@@ -1,2 +1,48 @@
 // Write your describeCity function here! ✨
 // You'll need to export it so the tests can run it.
+
+interface CityObject {
+	name: string;
+	catchphrase?: string;
+	coordinates: {
+		north: (number | string)[];
+		west: (number | string)[];
+	};
+}
+
+export type DescribeCityFunction = (cityObject: CityObject) => string;
+
+export const describeCity: DescribeCityFunction = (cityObject: CityObject) => {
+	let string = "";
+	string = string.concat(cityObject.name + `, New York\n`);
+	if (cityObject?.catchphrase) {
+		string = string.concat('* "' + cityObject.catchphrase + '"\n');
+	}
+	let n = cityObject.coordinates.north;
+	let w = cityObject.coordinates.west;
+	n.forEach((num: number | string, index) => {
+		if (typeof num === "string") {
+			num = parseInt(num);
+		}
+		if (num < 10) {
+			let strNum = num.toString();
+			strNum = "0".concat(strNum);
+			n[index] = strNum;
+		}
+	});
+	w.forEach((num: number | string, index) => {
+		if (typeof num === "string") {
+			num = parseInt(num);
+		}
+		if (num < 10) {
+			let strNum = num.toString();
+			strNum = "0".concat(strNum);
+			w[index] = strNum;
+		}
+	});
+	string = string.concat(
+		"* Located at " + `${n[0]}°${n[1]}'${n[2]}"N ${w[0]}°${w[1]}'${w[2]}"W`
+	);
+
+	return string;
+};
