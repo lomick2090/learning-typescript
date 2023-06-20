52428568efa7817474156be788b28bd359c62909
lomick2090 <lomick2090@gmail.com>
2023-06-20 16:19:53 -0500

    Finish part 3 of creatures

--- projects/classes/classifying-creatures/03-hamster-helpers/index.ts
+++ projects/classes/classifying-creatures/03-hamster-helpers/index.ts
@@ -1,2 +1,62 @@
 // Write your type and classes here! ✨
 // You'll need to export the classes so the tests can run them.
+
+export type SmallPetFood =
+	| "bugs"
+	| "fruits"
+	| "insects"
+	| "plants"
+	| "seeds"
+	| "vegetables";
+
+export abstract class SmallFurryPet {
+	readonly species: string;
+	protected happiness: number;
+
+	constructor(species: string) {
+		this.species = species;
+		this.happiness = 0;
+	}
+
+	abstract eats(food: SmallPetFood): boolean;
+
+	isHappy() {
+		return this.happiness > 0;
+	}
+}
+
+export class Gerbil extends SmallFurryPet {
+	constructor(species: string = "Gerbil") {
+		super(species);
+	}
+
+	eats(food: SmallPetFood) {
+		if (
+			food == "insects" ||
+			food == "plants" ||
+			food == "seeds" ||
+			food == "vegetables"
+		) {
+			return true;
+		}
+		return false;
+	}
+
+	dig() {
+		this.happiness = this.happiness + 1;
+	}
+}
+
+export class Hamster extends SmallFurryPet {
+	constructor(species: string = "Hamster") {
+		super(species);
+	}
+
+	eats(food: SmallPetFood) {
+		return true;
+	}
+
+	run() {
+		this.happiness = this.happiness + 1;
+	}
+}
