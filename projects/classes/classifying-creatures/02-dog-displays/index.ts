ff71260fefa69867affa192b832fdc1eba2bb444
lomick2090 <lomick2090@gmail.com>
2023-06-20 16:01:02 -0500

    finish puppy problem

--- projects/classes/classifying-creatures/02-dog-displays/index.ts
+++ projects/classes/classifying-creatures/02-dog-displays/index.ts
@@ -1,2 +1,25 @@
-// Write your class and interface here! ✨
-// You'll need to export it so the tests can run it.
+export interface PuppyInTheWindow {
+	colors: string[];
+	furriness: number;
+	owner: string | undefined;
+}
+
+export class Puppy implements PuppyInTheWindow {
+	colors: string[];
+	furriness: number;
+	owner: string | undefined;
+
+	constructor(
+		colors: string[],
+		furriness: number,
+		owner: string | undefined = undefined
+	) {
+		this.colors = colors;
+		this.furriness = furriness;
+		this.owner = owner;
+	}
+
+	adopt(newOwner: string) {
+		this.owner = newOwner;
+	}
+}
