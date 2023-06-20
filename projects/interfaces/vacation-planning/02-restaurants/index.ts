232104dd1038d18bfc148a2cc6b4b9bf40edcd2e
lomick2090 <lomick2090@gmail.com>
2023-06-20 13:10:59 -0500

    Finish Vacation Planning 2

--- projects/interfaces/vacation-planning/02-restaurants/index.ts
+++ projects/interfaces/vacation-planning/02-restaurants/index.ts
@@ -1,2 +1,34 @@
 // Write your groupRestaurants function here! ✨
 // You'll need to export it so the tests can run it.
+
+interface Restaurant {
+	city: string;
+	name: string;
+}
+
+export type groupFunction = (restArr: Restaurant[]) => {};
+
+export const groupRestaurants: groupFunction = (restArr: Restaurant[]) => {
+	let result = {};
+	let shallowCity: string[] = [];
+	restArr.forEach((rest) => {
+		shallowCity.push(rest.city);
+	});
+	let uniqueCity = [...new Set(shallowCity)];
+
+	uniqueCity.forEach((city) => {
+		let cityRestArr: string[] = [];
+		let filteredRestArr = restArr.filter((rest) => {
+			return rest.city === city;
+		});
+		filteredRestArr.forEach((rest) => {
+			cityRestArr.push(rest.name);
+		});
+		result = {
+			...result,
+			[city]: cityRestArr,
+		};
+	});
+
+	return result;
+};
