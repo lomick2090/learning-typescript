30f552d9482c7e92bbf6d05aadf820ee24747e8d
lomick2090 <lomick2090@gmail.com>
2023-06-20 13:51:52 -0500

    Finish vaction planning 3

--- projects/interfaces/vacation-planning/03-landmarks/index.ts
+++ projects/interfaces/vacation-planning/03-landmarks/index.ts
@@ -1,2 +1,87 @@
 // Write your describeLandmark function here! ✨
 // You'll need to export it so the tests can run it.
+
+export interface LandmarkBase {
+	name: string;
+}
+
+export interface HeightLandmark extends LandmarkBase {
+	height: number;
+}
+
+export interface Lighthouse extends HeightLandmark {
+	lit: number;
+	type: "lighthouse";
+}
+
+export interface Waterfall extends HeightLandmark {
+	type: "waterfall";
+}
+
+export interface Mountain extends HeightLandmark {
+	type: "mountain";
+}
+
+export interface Park extends LandmarkBase {
+	acres: number;
+	type: "park";
+}
+
+export interface River extends LandmarkBase {
+	depth?: number;
+	length: number;
+	type: "river";
+}
+
+export interface Lake extends LandmarkBase {
+	miles: number;
+	type: "lake";
+}
+
+export interface Fort extends LandmarkBase {
+	type: "fort";
+}
+
+export type Landmark =
+	| Fort
+	| Lake
+	| River
+	| Park
+	| Mountain
+	| Waterfall
+	| Lighthouse;
+
+export type DescribeLandmarkFunc = (landmark: Landmark) => string;
+
+export const describeLandmark: DescribeLandmarkFunc = (landmark: Landmark) => {
+	let string = `${landmark.name} is a ${landmark.type} in Upstate New York.`;
+
+	switch (landmark.type) {
+		case "lake":
+			string = string + `\nIt covers ${landmark.miles} square miles of water.`;
+			break;
+		case "lighthouse":
+			string =
+				string +
+				`\nIt was first lit in ${landmark.lit} and is ${landmark.height} feet high.`;
+			break;
+		case "mountain":
+			string = string + `\nIts peak is ${landmark.height} feet high.`;
+			break;
+		case "park":
+			string = string + `\nIt covers ${landmark.acres} square acres.`;
+			break;
+		case "river":
+			string =
+				string +
+				`\nIt flows for ${landmark.length} miles and is ${landmark.depth} feet deep at its deepest.`;
+			break;
+		case "waterfall":
+			string = string + `\nIt is ${landmark.height} feet high.`;
+			break;
+		default:
+			break;
+	}
+
+	return string;
+};
