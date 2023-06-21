c83b2f681b82996335a920565a5e1cc0363d57cd
lomick2090 <lomick2090@gmail.com>
2023-06-21 13:18:55 -0500

    finish 01 type modifiers

--- projects/type-modifiers/modifiers-of-the-types/01-fabulous-secret-powers/index.ts
+++ projects/type-modifiers/modifiers-of-the-types/01-fabulous-secret-powers/index.ts
@@ -2,3 +2,23 @@ import { characters } from "./characters";
 
 // Write your announceCharacter function here! ✨
 // You'll need to export it so the tests can run it.
+export interface Character {
+	name: string;
+	powers: string[];
+	side: "good" | "evil";
+}
+
+export function announceCharacter(rawCharText: string): Character {
+	let char = JSON.parse(rawCharText);
+	let powers = char.powers.join(", ");
+
+	console.log(`I am ${char.name}.`);
+	console.log(`My powers are: ${powers}.`);
+	console.log(`I am ${char.side}.`);
+
+	return {
+		name: char.name,
+		powers: char.powers,
+		side: char.side,
+	};
+}
