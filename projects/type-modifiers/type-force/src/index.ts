b61ef5e0dedfcdafecf9650285e12c9a2abb211f
lomick2090 <lomick2090@gmail.com>
2023-06-21 14:34:23 -0500

    Finish type-force

--- projects/type-modifiers/type-force/src/index.ts
+++ projects/type-modifiers/type-force/src/index.ts
@@ -1,31 +1,46 @@
 // Write your duel function and types below! ✨
 // You'll need to export duel so the tests can run it.
 
+export interface Character {
+	flying: boolean;
+	name: string;
+	power: number;
+	toughness: number;
+}
+
+export type Mutations =
+	| "energy"
+	| "healing"
+	| "luck"
+	| "flight"
+	| "strength"
+	| "wings";
+
 const mutationsLibrary = {
-	energy: (hero) => {
+	energy: (hero: Character) => {
 		hero.power *= 1.25;
 		hero.flying = true;
 	},
-	healing: (hero) => {
+	healing: (hero: Character) => {
 		hero.toughness *= 2;
 	},
-	luck: (hero) => {
+	luck: (hero: Character) => {
 		hero.power *= 1.25;
 		hero.toughness *= 1.25;
 	},
-	flight: (hero) => {
+	flight: (hero: Character) => {
 		hero.flying = true;
 	},
-	strength: (hero) => {
+	strength: (hero: Character) => {
 		hero.power *= 2;
 	},
-	wings: (hero) => {
+	wings: (hero: Character) => {
 		hero.flying = true;
 		hero.toughness *= 0.9;
 	},
 };
 
-function createCharacter(name, mutations) {
+function createCharacter(name: string, mutations: Mutations[]) {
 	const character = {
 		flying: false,
 		name,
@@ -38,4 +53,22 @@ function createCharacter(name, mutations) {
 	}
 
 	return character;
+}
+
+export interface DuelParams {
+	mutations: Mutations[];
+	name: string;
+}
+
+export type ReturnTuple = ["hero" | "villain", Character];
+
+export function duel(good: DuelParams, bad: DuelParams): ReturnTuple {
+	const goodCharacter = createCharacter(good.name, good.mutations);
+	const badCharacter = createCharacter(bad.name, bad.mutations);
+
+	const fightOutcome =
+		goodCharacter.power / badCharacter.toughness >=
+		badCharacter.power / goodCharacter.toughness;
+
+	return fightOutcome ? ["hero", goodCharacter] : ["villain", badCharacter];
 }
