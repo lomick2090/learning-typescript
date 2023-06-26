7559f6c2bf192da51313364ef8073f90fe4d9eb5
lomick2090 <lomick2090@gmail.com>
2023-06-26 12:26:48 -0500

    finish typespeare

--- projects/syntax-extensions/typespeare/02-much-ado-about-enums/colors.ts
+++ projects/syntax-extensions/typespeare/02-much-ado-about-enums/colors.ts
@@ -1,7 +1,7 @@
-export const Colors = {
-	Blue: "blue",
-	Red: "red",
-	Yellow: "yellow",
-} as const;
+export enum Colors {
+	Blue = "blue",
+	Red = "red",
+	Yellow = "yellow",
+}
 
 export type Color = typeof Colors[keyof typeof Colors];
